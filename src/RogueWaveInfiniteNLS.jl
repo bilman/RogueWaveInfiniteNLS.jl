547580ce5d70a850341597cd957ae76e43341634
Deniz Bilman <denizbilman@Denizs-MacBook-Air.local>
2024-05-23 23:42:37 +0300

    Adding exports to the main file again.

--- src/RogueWaveInfiniteNLS.jl
+++ src/RogueWaveInfiniteNLS.jl
@@ -1,6 +1,5 @@
 # SPDX-License-Identifier: MIT
 module RogueWaveInfiniteNLS
-export psi, psi_nodeformation_rescaled, psi_largeX, psi_largeT, psi_Painleve, rwio_nodeformation_rescaled, rwio_largeX, rwio_largeT, rwio_Painleve, rwio_nodeformation_rescaled_RHP, rwio_largeX_RHP, rwio_largeT_RHP, rwio_Painleve_RHP
 
 using LinearAlgebra
 using OperatorApproximation
@@ -10,6 +9,7 @@ using Polynomials
 include("rwio_RHP.jl")
 include("rwio.jl")
 
+export psi, psi_nodeformation_rescaled, psi_largeX, psi_largeT, psi_Painleve, rwio_nodeformation_rescaled, rwio_largeX, rwio_largeT, rwio_Painleve, rwio_nodeformation_rescaled_RHP, rwio_largeX_RHP, rwio_largeT_RHP, rwio_Painleve_RHP
 
 
 
