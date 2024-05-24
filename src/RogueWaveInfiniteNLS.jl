d313dfbd06d9fbd665737ed9e3fe51befaebcac5
Deniz Bilman <denizbilman@Denizs-MacBook-Air.local>
2024-05-24 10:15:56 +0300

    Adding more exports.

--- src/RogueWaveInfiniteNLS.jl
+++ src/RogueWaveInfiniteNLS.jl
@@ -9,7 +9,9 @@ using Polynomials
 include("rwio_RHP.jl")
 include("rwio.jl")
 
-export psi, psi_nodeformation_rescaled, psi_largeX, psi_largeT, psi_Painleve, rwio_nodeformation_rescaled, rwio_largeX, rwio_largeT, rwio_Painleve, rwio_nodeformation_rescaled_RHP, rwio_largeX_RHP, rwio_largeT_RHP, rwio_Painleve_RHP
+export psi, psi_nodeformation_rescaled, psi_largeX, psi_largeT, psi_Painleve
+export rwio_nodeformation_rescaled, rwio_largeX, rwio_largeT, rwio_Painleve, rwio_nodeformation_rescaled_RHP, rwio_largeX_RHP, rwio_largeT_RHP, rwio_Painleve_RHP
+export vfromTw, vfromXT, wfromXT, Tmax_givenX, Xmax_givenT, XfromTw
 
 
 
