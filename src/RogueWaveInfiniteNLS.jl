d5807b1e8fec16be26f26f62b6d9cf188a456602
Deniz Bilman <denizbilman@Denizs-MacBook-Air.local>
2024-06-11 13:27:51 +0300

    Routine to compute the slice at T=0.

--- src/RogueWaveInfiniteNLS.jl
+++ src/RogueWaveInfiniteNLS.jl
@@ -9,7 +9,7 @@ using Polynomials
 include("rwio_RHP.jl")
 include("rwio.jl")
 
-export psi, psi_nodeformation_rescaled, psi_largeX, psi_largeT, psi_Painleve
+export psi, psi_nodeformation_rescaled, psi_largeX, psi_largeT, psi_Painleve, Psi_slice_T0
 export rwio_nodeformation_rescaled, rwio_largeX, rwio_largeT, rwio_Painleve, rwio_nodeformation_rescaled_RHP, rwio_largeX_RHP, rwio_largeT_RHP, rwio_Painleve_RHP
 export vfromTw, vfromXT, wfromXT, Tmax_givenX, Xmax_givenT, XfromTw, TfromXv
 export NODEF_PTS, LARGEX_PTS, LARGET_PTS, VCRIT, WCRIT
